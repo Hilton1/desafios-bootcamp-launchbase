71412f5285e2f47cdead986bae7c7b593e3b75ca
Hilton1 <josehilton02@gmail.com>
2021-05-03 15:48:30 -0300

    Adding 3-2 challenge

---
+++ Docs/Semana01/modulo03/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: "mapas-com-react-usando-leaflet",
+    image_url: "https://blog.rocketseat.com.br/content/images/2020/11/blog-thumb-utilizando-mapas-no-react-com-leaflet-1.jpg",
+    title: "Mapas com React Usando Leaflet",
+    author: "Thiago Marinho",
+    duration: "11 min"
+  },
+  {
+    id: "construindo-um-app-com-mapas-usando-react-native-maps-e-mapbox",
+    image_url: "https://blog.rocketseat.com.br/content/images/2020/10/teste.jpg",
+    title: "Construindo App com Mapa usando React Native Maps e MapBox",
+    author: "Thiago Marinho",
+    duration: "8 min"
+  },
+  {
+    id: "upload-de-imagens-no-front-end-com-react-js-e-context-api-3",
+    image_url: "https://blog.rocketseat.com.br/content/images/2020/09/upload-de-imagens-no-s3-da-aws-com-node-js.png",
+    title: "Upload de imagens no S3 da AWS com Node.js",
+    author: "Thiago Marinho",
+    duration: "17 min"
+  }
+]
--- Docs/Semana01/modulo03/server.js
+++ Docs/Semana01/modulo03/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express();
+const courses = require('./data')
 
 server.use(express.static('public'))
 
@@ -12,11 +13,30 @@ nunjucks.configure('views', {
 })
 
 server.get('/', function(req, res) {
-  res.render('courses')
+  res.render('courses', {courses})
 })
 
 server.get('/about', function(req, res) {
-  res.render("about")
+  const about = {
+    image_url: "https://pbs.twimg.com/profile_images/1291682473592659968/sEorc6oh.jpg",
+    title: "Rocketseat",
+    description: "Evolua rápido como a tecnologia.",
+    subdescription: "Junte-se a milhares de devs e acelere na direção dos seus objetivos.",
+    techs: [
+      {name: "React"},
+      {name: "React Native"},
+      {name: "Node"},
+      {name: "Elixir"},
+      {name: "Flutter"}
+    ],
+    links: [
+      {name: "Github", url: "https://github.com/Rocketseat"},
+      {name: "Instagram", url: "https://www.instagram.com/rocketseat_oficial/?hl=pt-br"},
+      {name: "Facebook", url: "https://pt-br.facebook.com/rocketseat/"}
+    ]
+  }
+
+  res.render("about", {about})
 })
 
 server.use(function(req, res) {
