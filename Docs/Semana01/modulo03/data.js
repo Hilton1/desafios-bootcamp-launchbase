7cccdd49b41008a6bca4a3fc871ea012e1404d87
Hilton1 <josehilton02@gmail.com>
2021-09-26 19:55:46 -0300

    Updating archives

--- Docs/Semana01/modulo03/data.js
+++ Docs/Semana01/modulo03/data.js
@@ -1,6 +1,5 @@
 module.exports = [
   {
-<<<<<<< HEAD
     id: "nextjs-novidades-na-versao-10-e-atualizacao-do-blog-para-melhorar-a-performance",
     image_url: "https://blog.rocketseat.com.br/content/images/2020/11/capa_visao-geral-sobre-next-js-na-versao-10-e-atualizacao-do-blog-para-melhorar-a-performance.jpg",
     title: "Next.JS - Novidades na versão 10 e atualização do blog para melhorar a performance",
@@ -22,26 +21,3 @@ module.exports = [
     duration: "2 MIN",
   }
 ];
-=======
-    id: "mapas-com-react-usando-leaflet",
-    image_url: "https://blog.rocketseat.com.br/content/images/2020/11/blog-thumb-utilizando-mapas-no-react-com-leaflet-1.jpg",
-    title: "Mapas com React Usando Leaflet",
-    author: "Thiago Marinho",
-    duration: "11 min"
-  },
-  {
-    id: "construindo-um-app-com-mapas-usando-react-native-maps-e-mapbox",
-    image_url: "https://blog.rocketseat.com.br/content/images/2020/10/teste.jpg",
-    title: "Construindo App com Mapa usando React Native Maps e MapBox",
-    author: "Thiago Marinho",
-    duration: "8 min"
-  },
-  {
-    id: "upload-de-imagens-no-front-end-com-react-js-e-context-api-3",
-    image_url: "https://blog.rocketseat.com.br/content/images/2020/09/upload-de-imagens-no-s3-da-aws-com-node-js.png",
-    title: "Upload de imagens no S3 da AWS com Node.js",
-    author: "Thiago Marinho",
-    duration: "17 min"
-  }
-]
->>>>>>> 71412f5285e2f47cdead986bae7c7b593e3b75ca
